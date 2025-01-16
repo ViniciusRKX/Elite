dd61803d3ec014b23f14dcac3b5c92ee8d137f9c
Vinicius José <vinicius.jdsilva98@gmail.com>
2025-01-16 00:35:51 -0300

    Finalização das fotos do carossel

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ window.onscroll = function() {
     header.classList.remove('scrolled'); 
   }
 };
+
+const menuIcon = document.querySelector('.bi');
+const menu = document.querySelector('.menu ul');
+
+menuIcon.addEventListener ('click', ()=> {
+  menu.classList.toggle('active');
+});
